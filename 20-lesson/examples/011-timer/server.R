97359979f5ed30b918551d953ded97c1f8befe6c
doctordo <do@berkeley.edu>
2016-08-09 12:13:13 -0700

    removed extraneous

--- 20-lesson/examples/011-timer/server.R
+++
@@ -1,9 +0,0 @@
-library(stringr)
-library(lubridate)
-
-shinyServer(function(input, output, session) {
-  output$currentTime <- renderText({
-    invalidateLater(1000, session)
-    str_c("The current time is ", now())
-  })
-})
